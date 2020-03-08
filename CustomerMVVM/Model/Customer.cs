bee0e099ac296398219929a92f890ddafd04ad4a
somtirthabag <somtirthabag@gmail.com>
2020-03-07 23:33:10 -0500

    Added all files

---
+++ CustomerMVVM/Model/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace CustomerMVVM.Model
+{
+    public class Customer : INotifyPropertyChanged
+    {
+        public Customer(String CustomerName)
+        {
+            Name = CustomerName;
+        }
+
+
+        public string _Name;
+        public string Name
+        {
+            get { return _Name; }
+            set { _Name = value;
+              }
+        }
+
+        //private void OnPropertyChanged(string v)
+        //{
+        //    throw new NotImplementedException();
+       // }
+
+        private readonly INotifyPropertyChanged Members;
+public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ CustomerMVVM/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomerMVVM.Model;
+using System.Diagnostics;
+
+namespace CustomerMVVM.ViewModel
+{
+    class CustomerViewModel
+    {
+       public CustomerViewModel()
+        {
+            _Customer = new Customer("Somie Bag");
+        }
+        private Customer _Customer;
+
+        public Customer Customer { get => _Customer; }
+
+        public void saveChanges()
+        {
+            Debug.Assert(false, String.Format("The {0} was updated", Customer.Name));
+        }
+    }
+}
